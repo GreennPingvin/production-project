02a08ef89c34c177f1b7e89a3006416a8c124f4f
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-06-01 13:00:12 +0300

    add Router and split code into chunks

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
-root.render(<App />)
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
